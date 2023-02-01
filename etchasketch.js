657dd1353d0e9e7c381b686ea85203530082563e
Jeremy <jip101@gmail.com>
2023-02-01 16:20:11 -0500

    add working solid, rainbow, erase functions

--- etchasketch.js
+++ etchasketch.js
@@ -1,19 +1,35 @@
 let container = document.querySelector('.container');
 let selected = document.querySelector('#colorWheel');
 
-for (i=1; i<=4096; i++) {
-    let div = document.createElement("div");
+let gridSize = document.querySelector('.container')
+let columns = prompt('Enter size of grid')
+gridSize.style.gridTemplateColumns = `repeat(${columns}, minmax(0, 1fr))`
+
+
+for (i=1; i<=columns**2; i++) {
+    div = document.createElement("div");
     div.classList.add('box');
     div.setAttribute('class', 'box')
-    div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = pickColor()
-    })
+    div.addEventListener('mouseover', pickColor)
     container.appendChild(div);
 }
 
 let boxes = document.querySelectorAll('.box')
+
+//set default for erase and clear buttons
 let defaultBackground = document.querySelector('.box').style.backgroundColor;
-pickColor = () => selected.value;
+
+function pickColor () {
+    if (selected.id === 'rainbow') {
+        this.style.backgroundColor = randomRGB();
+    }
+    else if (selected.id === 'colorWheel') {
+        this.style.backgroundColor = document.querySelector('#colorWheel').value
+    }
+    else if (selected.id === 'erase') {
+        this.style.backgroundColor = defaultBackground
+    }
+}
 
 let buttons = document.querySelectorAll('.selector')
 buttons.forEach(button => {
@@ -25,16 +41,14 @@ buttons.forEach(button => {
 
 document.querySelector('#clear').addEventListener('click', () => {
     boxes.forEach(box => box.style.backgroundColor = defaultBackground)
-    
 })
 
 
 let grid = document.querySelector('#grid')
-console.log(boxes)
 grid.addEventListener('click', () => {
     boxes.forEach(box => {
         if (grid.value == 'on') {
-            box.style.border='hidden'
+            box.style.border='none'
         }
         else {
         box.style.border='1px solid black'
@@ -46,3 +60,13 @@ grid.addEventListener('click', () => {
     else {
         grid.value = 'on'
     }})
+
+
+
+function randomNumber() {
+    return (Math.floor(Math.random()*256))
+}
+
+function randomRGB() {
+    return `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`
+}
