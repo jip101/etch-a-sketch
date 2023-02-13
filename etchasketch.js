62b72fd0d42074181531e21fbae99fa0e4b87875
Jeremy <jip101@gmail.com>
2023-02-13 11:40:54 -0500

    add grid size slider

--- etchasketch.js
+++ etchasketch.js
@@ -2,33 +2,55 @@ let container = document.querySelector('.container');
 let selected = document.querySelector('#colorWheel');
 document.querySelector('input').style.backgroundColor = 'yellow'
 
-let gridSize = document.querySelector('.container')
-let width = prompt('Enter size of grid')
-gridSize.style.gridTemplateColumns = `repeat(${width}, minmax(0, 1fr))`
+//let gridSize = document.querySelector('.container')
+let defaultBackground;
+let width = document.querySelector('#gridSize');
+width.addEventListener('input', changeSize);
+let sizeDisplay = document.querySelector('#gridSizeText')
+
+changeSize();
 
 
-for (i=1; i<=width**2; i++) {
-    div = document.createElement("div");
-    div.classList.add('box');
-    div.setAttribute('class', 'box')
-    div.addEventListener('mouseover', pickColor)
-    container.appendChild(div);
+function changeSize () {
+    sizeDisplay.innerHTML = width.value + 'x' + width.value;
+    container.innerHTML = '';
+
+    container.style.gridTemplateColumns = `repeat(${width.value}, minmax(0, 1fr))`
+
+    for (i=1; i<=width.value**2; i++) {
+        div = document.createElement("div");
+        div.classList.add('box');
+        div.setAttribute('class', 'box')
+        div.style.border='1px solid black'
+        div.addEventListener('mouseover', pickColor)
+        container.appendChild(div);
+    }
+    //set default for erase and clear buttons
+    defaultBackground = document.querySelector('.box').style.backgroundColor;
+
+    let grid = document.querySelector('#grid')
+    grid.style.backgroundColor = 'yellow'
+    grid.value = 'on'
 }
 
 let boxes = document.querySelectorAll('.box')
 
-//set default for erase and clear buttons
-let defaultBackground = document.querySelector('.box').style.backgroundColor;
 
 function pickColor () {
     if (selected.id === 'rainbow') {
         this.style.backgroundColor = randomRGB();
+        this.style.filter = '';
     }
     else if (selected.id === 'colorWheel') {
         this.style.backgroundColor = document.querySelector('#colorWheel').value
+        this.style.filter = '';
     }
     else if (selected.id === 'erase') {
-        this.style.backgroundColor = defaultBackground
+        this.style.backgroundColor = defaultBackground;
+        this.style.filter = '';
+    }
+    else if (selected.id === 'shader') {
+        this.style.filter = shader(this.style);
     }
 }
 
@@ -47,13 +69,18 @@ buttons.forEach(button => {
 
 
 document.querySelector('#clear').addEventListener('click', () => {
-    boxes.forEach(box => box.style.backgroundColor = defaultBackground)
+    boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+        box.style.backgroundColor = defaultBackground;
+        box.style.filter = '';
+    })
 })
 
 
 let grid = document.querySelector('#grid')
 grid.style.backgroundColor = 'yellow'
 grid.addEventListener('click', () => {
+    boxes = document.querySelectorAll('.box');
     boxes.forEach(box => {
         if (grid.value == 'on') {
             box.style.border='none'
@@ -77,4 +104,14 @@ function randomRGB() {
     return `rgb(${Math.floor(Math.random()*256)}, 
     ${Math.floor(Math.random()*256)}, 
     ${Math.floor(Math.random()*256)})`
+}
+
+
+//decrease brightness by 0.1 with each pass
+function shader(e) {
+    if (!e.filter) {
+        return `brightness(0.9)`
+    }
+    let x = (e.filter).slice(11,-1)
+    return `brightness(${x-0.1})`
 }
